d057146e84ce898707686a6a445f94919c87bd18
Rohan Malhi <rohan.s.malhi@gmail.com>
2022-01-26 21:19:58 -0800

    bing bone blankness gone

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from cgi import test
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+testSurface = pygame.Surface((100, 200))
+testSurface.fill('blue')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-    #draw and update all elements
+            exit()
+            break
+    
+    screen.blit(testSurface, (0, 0))
+
     pygame.display.update()
+    clock.tick(60)
