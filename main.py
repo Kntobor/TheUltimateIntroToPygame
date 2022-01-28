83257ad65b1289745e6aa92fd30d12b38ad5e4cd
Rohan Malhi <rohan.s.malhi@gmail.com>
2022-01-27 19:12:34 -0800

    Added full game score (time in seconds)

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
 from sys import exit
 
+def displayScore():
+    currentTime = pygame.time.get_ticks() - timeSpent
+    scoreSurface = font.render(f'{int(currentTime / 1000)}', False, 'black')
+    scoreRect = scoreSurface.get_rect(center = (400, 30))
+    screen.blit(scoreSurface, scoreRect)
+timeSpent = 0
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
@@ -10,8 +17,6 @@ font = pygame.font.Font('font\Pixeltype.ttf', 50)
 
 skySurface = pygame.image.load('graphics\Sky.png').convert()
 groundSurface = pygame.image.load('graphics\ground.png').convert()
-scoreSurface = font.render('0', False, 'black')
-scoreRect = scoreSurface.get_rect(center = (400, 30))
 
 snailSurface = pygame.image.load('graphics\snail\snail1.png').convert_alpha()
 snailRect = snailSurface.get_rect(bottom = 300, right = 750)
@@ -32,6 +37,7 @@ while True:
                 elif gameOver == True:
                     playerRect.bottom = 300
                     snailRect.right = 750
+                    timeSpent = pygame.time.get_ticks()
                     gameOver = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
@@ -40,18 +46,18 @@ while True:
                 elif gameOver == True:
                     playerRect.bottom = 300
                     snailRect.right = 750
+                    timeSpent = pygame.time.get_ticks
                     gameOver = False
     if gameOver == False:
         screen.blit(skySurface, (0, 0))
         screen.blit(groundSurface, (0, 300))
-        screen.blit(scoreSurface, scoreRect)
+        displayScore()
 
         snailRect.left -= 4
         if snailRect.right <= 0:
             snailRect.left = 810
         screen.blit(snailSurface, snailRect)
 
-        #Player
         playerGravity+= 1
         playerRect.bottom += playerGravity
         if playerRect.bottom >= 300:
