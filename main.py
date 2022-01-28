47f50adb904358d9bb4c8da01d25a97dca24ef7a
Rohan Malhi <rohan.s.malhi@gmail.com>
2022-01-27 19:54:22 -0800

    Added menu screen and some extra comments to define regions

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
 import pygame
 from sys import exit
+round = 1
 
+# Score system
+currentTime = 0
 def displayScore():
-    currentTime = int((pygame.time.get_ticks() - timeSpent )/ 1000)
-    scoreSurface = font.render(f'{currentTime}', False, 'black')
+    global currentTime
+    if gameOver == False:
+        currentTime = int((pygame.time.get_ticks() - timeSpent )/ 1000)
+        scoreSurface = font.render(f'Score: {currentTime}', False, 'black')
+    elif gameOver == True:
+        scoreSurface = font.render(f'Score: {currentTime}', False, 'white')
     scoreRect = scoreSurface.get_rect(center = (400, 30))
     screen.blit(scoreSurface, scoreRect)
 timeSpent = 0
 
+#PyGame setup
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-gameOver = False
+gameOver = True
 font = pygame.font.Font('font\Pixeltype.ttf', 50)
 
+# Background assets
 skySurface = pygame.image.load('graphics\Sky.png').convert()
 groundSurface = pygame.image.load('graphics\ground.png').convert()
 
+# Enemies
 snailSurface = pygame.image.load('graphics\snail\snail1.png').convert_alpha()
 snailRect = snailSurface.get_rect(bottom = 300, right = 750)
 
+# Player
 playerSurface = pygame.image.load('graphics\Player\player_walk_1.png')
 playerRect = playerSurface.get_rect(bottom = 300, left = 80)
 playerGravity = 0
 
+# Menu Screen
+menuCharacterSurface = pygame.image.load('graphics\Player\player_stand.png').convert_alpha()
+menuCharacterSurface = pygame.transform.scale(menuCharacterSurface, (136, 168))
+menuCharacterRect = menuCharacterSurface.get_rect(center = (400, 200))
+titleTextSurface = font.render('Runner', False, 'white')
+titleTextRect = titleTextSurface.get_rect(center = (400, 30))
+tutorialTextSurface = font.render('Click or press space to begin', False, 'white')
+tutorialTextRect = tutorialTextSurface.get_rect(center = (400, 300))
+
+# Game Loop
 while True:
+    # Event Checker
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
                 pygame.quit()
@@ -48,6 +70,7 @@ while True:
                     snailRect.right = 750
                     timeSpent = pygame.time.get_ticks
                     gameOver = False
+                    
     if gameOver == False:
         screen.blit(skySurface, (0, 0))
         screen.blit(groundSurface, (0, 300))
@@ -65,9 +88,16 @@ while True:
         screen.blit(playerSurface, playerRect)
 
         if playerRect.colliderect(snailRect):
+            round += 1
             gameOver = True
     elif gameOver == True:
-        screen.fill('blue')
+        screen.fill((94, 129, 162))
+        screen.blit(menuCharacterSurface, menuCharacterRect)
+        if round != 1:
+            displayScore()
+        elif round == 1:
+            screen.blit(titleTextSurface, titleTextRect)
+        screen.blit(tutorialTextSurface, tutorialTextRect)
 
     pygame.display.update()
     clock.tick(60)
